6aa77d5530f6bccd9b70855bc748c3d2e6f4f7a2
NBP77 <nbp77@outlook.com>
2021-05-05 23:07:07 -0400

    Added the footer with styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import "./style.css";
 
 function Footer() {
   return (
     <div>
-
-       
-   
+      <footer>
+        <h4>Contact Me</h4>
+        <ul class="contact" id="contact">
+          <li>(555) 555-555</li>
+          <li>
+            <a
+              href="https://onedrive.live.com/?cid=19EB23398767F6CF&id=19EB23398767F6CF%2118396&parId=19EB23398767F6CF%21307&o=OneUp"
+              target="_blank"
+            >
+              Resume
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/nbp77" target="_blank">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/nick-pendergast-273014123/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
