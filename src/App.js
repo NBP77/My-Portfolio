45b8dd691e72fcfa1fe1bf1ce21b1e4188e3141c
NBP77 <nbp77@outlook.com>
2021-05-05 20:05:13 -0400

    Got the intial files and React env set up

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    
+  return (
+    <div
+      className="d-flex justify-content-center fadeOut align-items-center"
+      style={styles.div}
+    >
+      <h1 className="welcome text-center    col" style={styles.welcome}>
+        hello.
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
